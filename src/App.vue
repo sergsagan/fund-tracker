7ec190275ed5b475103a72f0a870057c295f1956
sergsagan <sergey.martynuk@gmail.com>
2024-08-27 14:00:36 +0200

    Balance, Income, Expenses and TransactionList

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,46 @@
   import IncomeExpenses from "@/components/IncomeExpenses.vue";
   import TransactionList from "@/components/TransactionList.vue";
   import AddTransaction from "@/components/AddTransaction.vue";
+
+  import { ref, computed, onMounted } from 'vue';
+
+  const transactions = ref([
+    {id: 1, text: 'Flower', amount: -19.00 },
+    {id: 2, text: 'Salary', amount: 299.00 },
+    {id: 3, text: 'Book', amount: -10 },
+    {id: 4, text: 'Camera', amount: -150 },
+  ]);
+
+  // Get total
+  const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+  });
+
+  // Get income
+  const income = computed(() => {
+    return transactions.value
+        .filter((transaction) => transaction.amount > 0)
+        .reduce((acc, transaction) => acc + transaction.amount, 0)
+        .toFixed(2);
+  });
+
+  // Get expenses
+  const expenses = computed(() => {
+    return transactions.value
+        .filter((transaction) => transaction.amount < 0)
+        .reduce((acc, transaction) => acc + transaction.amount, 0)
+        .toFixed(2);
+  });
+
 </script>
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpenses />
-    <TransactionList />
+    <Balance :total="total" />
+    <IncomeExpenses :income="income" :expenses="expenses" />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { defineProps } from 'vue';
 
+defineProps({
+  total: {
+    type: Number,
+    required: true,
+  }
+})
 </script>
 
 <template>
   <h4>Your Balance</h4>
-  <h1 id="balance">$0.00</h1>
+  <h1 id="balance">${{ total }}</h1>
 </template>
 
 <style scoped>
